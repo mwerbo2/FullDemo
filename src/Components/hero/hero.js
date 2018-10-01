0ab437c8726f546f4abf4c4261bf8441fd505f1b
Kenny <khuynh024@gmail.com>
2018-10-01 13:41:33 -0500

    added on hero image and tint

---
+++ src/Components/hero/hero.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './hero.css';
+
+class Hero extends Component {
+    render() {
+      return (
+        <div className="container hero tint">
+            <h1>DevSmithOne</h1>
+            <h3>Building your web and mobile products while training future web developer professionals</h3>
+
+            <button>Get in Touch</button>
+
+       
+        </div>
+      );
+    }
+  }
+  
+  export default Hero;
+  
