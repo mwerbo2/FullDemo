83cb1b2c8212c15283c9af4af15b053d0136724f
Kenny <khuynh024@gmail.com>
2018-10-02 11:44:50 -0500

    working on sidenav

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import Hero from './Components/hero/hero.js';
+import SideNav from './Components/sidenav/sidenav';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Hero/>
+
+        <SideNav/>
+                
       </div>
     );
   }
--- src/Components/sidenav/sidenav.js
+++ src/Components/sidenav/sidenav.js
@@ -1,16 +1,59 @@
 import React, { Component } from 'react';
 import './sidenav.css';
+import { Button, Header, Icon, Image, Menu, Segment, Sidebar, Container } from 'semantic-ui-react'
+import Hero from '../hero/hero'
 
 class SideNav extends Component {
-    render() {
-      return (
-        <div>
-          <h1>SideNav Here</h1>
-       
-        </div>
-      );
-    }
+  state = { visible: false }
+
+  handleButtonClick = () => this.setState({ visible: !this.state.visible })
+
+  handleSidebarHide = () => this.setState({ visible: false })
+
+  render(props) {
+    const { visible } = this.state
+
+    return (
+      <Container fluid>
+        <Sidebar.Pushable as={Segment}>
+          <Sidebar
+            as={Menu}
+            animation='overlay'
+            icon='labeled'
+            inverted
+            onHide={this.handleSidebarHide}
+            vertical
+            visible={visible}
+            width='thin'
+          >
+            <Menu.Item as='a'>
+              <Icon name='home' />
+              Home
+            </Menu.Item>
+            <Menu.Item as='a'>
+              <Icon name='gamepad' />
+              Games
+            </Menu.Item>
+            <Menu.Item as='a'>
+              <Icon name='camera' />
+              Channels
+            </Menu.Item>
+          </Sidebar>
+
+          <Sidebar.Pusher dimmed={visible}>
+            <Segment basic>
+            
+              <Hero/>
+              <Button onClick={this.handleButtonClick}>Toggle visibility</Button>
+
+            </Segment>
+          </Sidebar.Pusher>
+        </Sidebar.Pushable>
+      </Container>
+    )
   }
+}
+
+
   
-  export default SideNav;
-  
+export default SideNav;
