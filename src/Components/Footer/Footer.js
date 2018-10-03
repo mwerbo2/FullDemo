89d4ee0c466016fc201cc8f7d17d2401d00a783b
theRealGerph <jvongkoth@gmail.com>
2018-10-03 15:37:36 -0500

    fixed element tags of our location

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -7,26 +7,28 @@ const Footer = () => (
             <div className="ui container">
                 <div className="ui equal width grid">
                     <div className="column left-column">
-                        DevSmith
+                        <h3> DevSmith </h3>
               <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
                     </div>
                     <div className="column mid-column" >
-                        Our Location
+                        <h3> Our Location </h3>
             <p>
-                            <a target="_blank" className='footer-link' href="">111 N. Street Ave. Chicago, IL 11111</a>
+                            <p>111 N. Street Ave. Chicago, IL 11111</p>
                             <br></br>
-                            <a target="_blank" className='footer-link' href="">Phone Number: +1 (111) 111-1111</a>
+                            <p>Phone Number: +1 (111) 111-1111</p>
                             <br></br>
-                            <a target="_blank" className='footer-link' href="">Email: DevSmith@DevSmith.com</a>
+                            <p>Email: DevSmith@DevSmith.com</p>
                             <br></br>
                         </p>
                     </div>
-                    <div className="column right-column">Quick Links
-          <p id="code-text">
+                    <div className="column right-column">
+                    <h3> Quick Links </h3>
+          <p id="quickLinks">
                             <a target="_blank">Home</a>
+                            <br></br>
                             <a target="_blank">About</a>
+                            <br></br>
                             <a target="_blank">Contact</a>
-
                         </p>
 
                     </div>
