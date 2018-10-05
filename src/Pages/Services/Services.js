cd46d2b81654678a464009deff71270d07747edc
theRealGerph <jvongkoth@gmail.com>
2018-10-04 20:37:56 -0500

    added Services component

---
+++ src/Pages/Services/Services.js
@@ -0,0 +1,14 @@
+import React, { Component} from "react";
+import "./Services.css";
+
+class Services extends Component {
+
+    render() {
+
+        return{
+
+        }
+    };
+}
+
+export default Services;
