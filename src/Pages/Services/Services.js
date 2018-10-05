1764b41bcd247173c1de156075731ec5152619b5
theRealGerph <jvongkoth@gmail.com>
2018-10-04 20:55:35 -0500

    added semantic ui rail

--- src/Pages/Services/Services.js
+++ src/Pages/Services/Services.js
@@ -1,13 +1,28 @@
 import React, { Component} from "react";
 import "./Services.css";
+import { Grid, Image, Rail, Segment } from 'semantic-ui-react'
 
 class Services extends Component {
 
     render() {
 
-        return{
+        return(
+            <Grid centered columns={3}>
+    <Grid.Column>
+      <Segment>
+        <Image src='/images/wireframe/paragraph.png' />
 
-        }
+        <Rail dividing position='left'>
+          <Segment>Left Rail Content</Segment>
+        </Rail>
+
+        <Rail dividing position='right'>
+          <Segment>Right Rail Content</Segment>
+        </Rail>
+      </Segment>
+    </Grid.Column>
+  </Grid>
+        )
     };
 }
 
