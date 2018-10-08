3db3c25e571a1801c2a2d89064925e5d165838ae
Michael W <mwerbo2@gmail.com>
2018-10-08 17:12:09 -0500

    Contact us style

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -1,44 +1,54 @@
 import React from 'react'
-import { Button, Checkbox, Form, Grid, Header} from 'semantic-ui-react'
+import { Button, Checkbox, Form, Grid, Header, Container} from 'semantic-ui-react'
 
 
 
 const ContactUs = () => (
-<Grid divided='vertically'>
-  <Grid.Row  columns={2} textAlign={"left"}>
-  <Grid.Column textAlign="center" verticalAlign="middle" width={7} >
-    <Header>
-      Hello World
-    </Header>
-  </Grid.Column>
-    <Grid.Column verticalAlign="center" width={7} 
-      style={{   
-        border: "1px",
-        backgroundColor: "grey",      
-        marginTop: '20vh',
-        marginBottom: '0',
-        padding: "20px",
+<Grid stackable columns={2} divided='vertically'>
+  <Grid.Row  columns={2} textAlign={"left"}
+    style={{
+      marginBottom: "20vh"
+    }}>
+    <Grid.Column textAlign="center" verticalAlign="middle" width={7} 
+      style={{       
+        marginTop: '30vh',
         borderRadius: "5px"
       }}>
-       <Header>
-    Contact Us!
-  </Header>
+      <Header as="h1">
+        Start a Project Today
+      </Header>
+      <Header as="h2">
+        Send a message or give us a call for a free consult.
+      </Header>
+      <Container
+        style={{   
+          fontSize:"22px"
+        }}>
+        <p>Chicago, Il <br/>
+        +1 630-634-5176<br/>
+        </p>
+      </Container>
+    </Grid.Column>
+    <Grid.Column textAlign="left" verticalAlign="left" width={7} 
+      style={{        
+        marginTop: '30vh'
+      }}>
+      <Header>
+        Contact Us!
+      </Header>
       <Form>
         <Form.Group>
-          <Form.Field width={7}>
+          <Form.Field width={8}>
             <label>Name</label>
             <input placeholder='Name' />
           </Form.Field>
-          <Form.Field width={7}>
+          <Form.Field width={8}>
             <label>Email</label>
             <input placeholder='Email' />
           </Form.Field>
         </Form.Group>
         <Form.TextArea label='About' placeholder='Tell us more about you...' />
-        <Form.Field>
-          <Checkbox label='I agree to the Terms and Conditions' />
-        </Form.Field>
-        <Button type='submit'>Submit</Button>
+          <Button type='submit'>Submit</Button>
       </Form>
     </Grid.Column>  
   </Grid.Row>
